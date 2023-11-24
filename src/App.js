b58ef5294cff9f5cf19779e450db77be2c2e5329
tiaguinzero <tiago.cps123@gmail.com>
2023-11-24 16:56:10 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HeaderComponent from './components/header/header';
 import NavComponent from './components/nav/nav';
 import { Outlet } from 'react-router-dom';
+import FooterComponet from './components/footer/footer'
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
             <HeaderComponent/>
             <NavComponent/>
             <Outlet/>
-            <h1>Footer</h1>
+            <FooterComponet/>
         </div>
     )
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,31 @@
 import style from './footer.module.css'
+import logo from '../../img/logo.png'
+import whatsapp from '../../img/whatsapp.png'
+import twitter from '../../img/twitter.png'
+import gitHub from '../../img/gitHub.png'
+import instagram from '../../img/Instagram.png'
 
 function FooterComponent (){
     return(
-        <div className="rodape">
-
-        </div>
+        <footer>
+            <div className={style.left}>
+                <img src={logo}/>
+                <p>R. Culto à Ciência, 177 - Centro, Campinas - SP, 13020-060</p>
+            </div>
+            <div className={style.right}>
+                <div className={style.redesSocias}>
+                    <a href='https://www.whatsapp.com/?lang=pt_BR'><img src={whatsapp}/></a>
+                    <a href='https://twitter.com/'><img src={twitter}/></a>
+                    <a href='https://github.com/Mesuring'><img src={gitHub}/></a>
+                    <a href='https://www.instagram.com/'><img src={instagram}/></a>
+                </div>
+                <div className={style.direitos}>
+                    <p>© todos os  direitos reservado para a Measuring</p>
+                </div>
+            </div>
+            
+        
+        </footer>
     )
 }
 
