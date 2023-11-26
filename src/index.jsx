c17e5cd6145df212f42d0aba05cb6e6981100190
tiaguinzero <tiago.cps123@gmail.com>
2023-11-26 17:48:33 -0300

    alterando estrutura das rotas

--- src/index.jsx
+++ src/index.jsx
@@ -12,57 +12,32 @@ import BuquePage from './pages/buque-flor/buque';
 import CestaPage from './pages/cesta-de-flores/cesta';
 import PresentesPage from './pages/presentes/presentes';
 import VazoPage from './pages/vazo-de-flores/vazo';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <ErroPage/>,
-    children:[{
-      path: "/",
-      element: <HomePage/>
-    },
-    {
-      path: "/:id",
-      element: <ProdutoPage/>
-    },
-    {
-      path: "/Buque-flores",
-      element: <BuquePage/>
-    },
-    {
-      path: "/Arranjo-de-flores",
-      element: <ArranjoPage/>
-    },
-    {
-      path: "/Vazo-de-flores",
-      element: <VazoPage/>
-    },
-    {
-      path: "/Cesta-de-flores",
-      element: <CestaPage/>
-    },
-    {
-      path: "/Presentes",
-      element: <PresentesPage/>
-    }
-    ]
-  },
-  {
-    path: "/Cadastro",
-    element: <CadastroPage/>
-  },
-  {
-    path: "/Login",
-    element: <LoginPage/>
-  }
-])
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path=":id" element={<ProdutoPage />} />
+        <Route path="Buque-flores" element={<BuquePage />} />
+        <Route path="Arranjo-de-flores" element={<ArranjoPage />} />
+        <Route path="Vazo-de-flores" element={<VazoPage />} />
+        <Route path="Cesta-de-flores" element={<CestaPage />} />
+        <Route path="Presentes" element={<PresentesPage />} />
+      </Route>
+      <Route path="/Cadastro" element={<CadastroPage />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="*" element={<ErroPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={Router}/>
+    <Router />
   </React.StrictMode>
 );
 
