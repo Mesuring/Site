ce669c53dbe6c59a0a084ca12dcf2fe40076bc6a
tiaguinzero <tiago.cps123@gmail.com>
2023-12-03 14:42:30 -0300

    ✅: add validação page cadastro.

--- src/components/cadastro/cadastro.jsx
+++ src/components/cadastro/cadastro.jsx
@@ -1,52 +1,70 @@
-import blogFetch from "../../axios/config";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom/dist";
-import { Link } from 'react-router-dom'
-import style from './cadastro.module.css'
+import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form'; 
 
-function CadastroComponent(){
-    const navigate = useNavigate()
-    const [nome, setNome] = useState();
-    const [cpf, setCpf] = useState();
-    const [aniversario, setAniversario] = useState();
-    const [cep, setCep] = useState();
-    const [email, setEmail] = useState();
-    const [senha, setSenha] = useState();
+import style from './cadastro.module.css';
 
-    const criaCadastro = (e) => {
-        e.preventDefault();
-        
-        console.log('Dados enviados:', { nome, cpf, aniversario, cep, email, senha })
-    }
-    return(
+const validation = yup.object().shape({
+    nome: yup.string().required(),
+    cpf: yup.number().required(),
+    aniversario: yup.date().required(),
+    cep: yup.number().required(),
+    email: yup.string().required(),
+    senha: yup.string().required()
+});
+
+function CadastroComponent() {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(validation),
+    });
+
+    const [errorMessage, setErrorMessage] = React.useState('');
+
+    const criaCadastro = (data) => {
+        // verifica se há erros antes de enviar os dados
+        if (Object.keys(errors).length === 0) {
+        console.log('Dados enviados:', data);
+        setErrorMessage(''); // Limpa a mensagem de erro se não houver erros
+        } else {
+        setErrorMessage('Preencha todos os campos.');
+        }
+    }; 
+
+    return (
         <main>
-            <form onSubmit={(e) => criaCadastro(e)}>
-                <div className={style.box}>
-                    <h1>Criar conta</h1>
-                    <label>Nome</label>
-                    <input type="text" id={style.nome} onChange={(e) => setNome(e.target.value)}/>
-                    
-                    <label>CPF</label>
-                    <input type="number" id={style.cpf} onChange={(e) => setCpf(e.target.value)}/>
-                    
-                    <label>Aniversário</label>
-                    <input type="date" id={style.aniversario} onChange={(e) => setAniversario(e.target.value)}/>
-                    
-                    <label>Cep</label>
-                    <input type="number" id={style.cep} onChange={(e) => setCep(e.target.value)}/>
+        <form onSubmit={handleSubmit(criaCadastro)}>
+            <div className={style.box}>
+            <h1>Criar conta</h1>
+            <label>Nome</label>
+            <input type="text" id={style.nome} {...register('nome')} />
 
-                    <label>E-mail</label>
-                    <input type="email" id={style.email} onChange={(e) => setEmail(e.target.value)}/>
+            <label>CPF</label>
+            <input type="number" id={style.cpf} {...register('cpf')} />
 
-                    <label>Senha</label>
-                    <input type="password" id={style.senha} onChange={(e) => setSenha(e.target.value)}/>
-                        
-                    <button type="submit">Criar</button>
-                    <p>Já possui conta? <Link to="/Login">Login</Link></p>
-                </div>
-            </form>
+            <label>Aniversário</label>
+            <input type="date" id={style.aniversario} {...register('aniversario')} />
+
+            <label>Cep</label>
+            <input type="number" id={style.cep} {...register('cep')} />
+
+            <label>E-mail</label>
+            <input type="email" id={style.email} {...register('email')} />
+
+            <label>Senha</label>
+            <input type="password" id={style.senha} {...register('senha')} />
+
+            <p className={style.erroMessage}>
+                {Object.values(errors).length > 0 && 'Preencha todos os campos'}
+            </p>
+
+            <button type="submit">Criar</button>
+
+            </div>
+        </form>
         </main>
-    )
+    );
 }
 
-export default CadastroComponent
+export default CadastroComponent;
